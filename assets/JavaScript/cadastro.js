8870792c930361d2e7e1f391f920eeb934c779d5
pedroteixeira1998 <pteixeira98@live.com>
2023-04-28 12:38:14 -0300

    Fazer calculo  valores custo e venda e exibir lucro

--- assets/JavaScript/cadastro.js
+++ assets/JavaScript/cadastro.js
@@ -2,15 +2,16 @@ const precodecusto = document.querySelector(".original-price");
 const precodevenda = document.querySelector(".sale-price");
 const precoFinalElement = document.querySelector('.preco-final');
 
+
 function somaprecofinal(){
     const precoCusto = precodecusto.value;
     const precoVenda = precodevenda.value;
     if(precoCusto !== "" && precoVenda !== ""){
-        const precoFinal = +precoCusto - +precoVenda;
+        const precoFinal = (+precoVenda - +precoCusto).toFixed(2);
         console.log(precoFinal);
-        precoFinalElement.textContent = `Preço Final: ${precoFinal}`;
-    }
-}
+        precoFinalElement.textContent = `R$${precoFinal}`;
+}}
+
 
 precodecusto.addEventListener("input", somaprecofinal);
 precodevenda.addEventListener("input", somaprecofinal);

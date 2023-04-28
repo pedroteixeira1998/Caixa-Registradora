fb95ed9c725a4164fc869bd9397bd54a5fb833a8
pedroteixeira1998 <pteixeira98@live.com>
2023-04-28 13:08:51 -0300

    Adicionar dados dos produtos ao array e salvar LS

--- assets/JavaScript/cadastro.js
+++ assets/JavaScript/cadastro.js
@@ -1,16 +1,60 @@
 const precodecusto = document.querySelector(".original-price");
 const precodevenda = document.querySelector(".sale-price");
 const precoFinalElement = document.querySelector('.preco-final');
+const cadastrar = document.querySelector('.form-button');
 
-
+// Cálculo valor do Lucro
 function somaprecofinal(){
     const precoCusto = precodecusto.value;
     const precoVenda = precodevenda.value;
     if(precoCusto !== "" && precoVenda !== ""){
         const precoFinal = (+precoVenda - +precoCusto).toFixed(2);
-        console.log(precoFinal);
         precoFinalElement.textContent = `R$${precoFinal}`;
 }}
+
+// Adicionar elementos ao array
+
+let dadosMarcados = [];
+
+cadastrar.addEventListener('click', function(){
+    const name = document.querySelector('.name').value;
+    const custo = precodecusto.value;
+    const venda = precodevenda.value;
+    const estoque = document.querySelector('.estoque').value;
+    const codsis = document.querySelector('.codsis').value;
+    const codprod = document.querySelector('.codprod').value;
+    const fornecedor = document.querySelector('.fornecedor').value;
+    const marca = document.querySelector('.marca').value;
+    const validade = document.querySelector('.validade').value;
+    const lucro = document.querySelector('.lucro').textContent;
+    const inputs = document.querySelectorAll('input');
+
+
+
+    const dados = {
+        nome: name,
+        custo: custo,
+        venda: venda,
+        estoque: estoque,
+        codsis: codsis,
+        codprod:codprod,
+        fornecedor:fornecedor,
+        marca:marca,
+        validade:validade,
+        lucro:lucro
+}
+
+    dadosMarcados.push(dados);
+    console.log(dadosMarcados);
+
+    localStorage.setItem('Produtos', dadosMarcados);
+
+    inputs.forEach(input => {
+        input.value = '';
+      });
+    
+})
+
 
 
 precodecusto.addEventListener("input", somaprecofinal);
